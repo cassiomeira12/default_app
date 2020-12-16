ba010ab797290363004ced27ee5fae606c8a27bb
Cássio Meira Silva <cassiomeira_12@hotmail.com>
2020-12-15 23:11:38 -0300

    fix: verification if user has company

--- lib/app/modules/company/company_controller.dart
+++ lib/app/modules/company/company_controller.dart
@@ -1,9 +1,17 @@
 import 'package:default_app/app/shared/repositories/parse_user_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CompanyController {
   var service = ParseUserService();
 
-  currentUser() {
+  currentUser() async {
+    var currentUser = await service.currentUser();
+    var pref = await SharedPreferences.getInstance();
+    var hasCompany = pref.getBool("hasCompany");
+    if (!hasCompany) {
+      await currentUser.logout();
+      throw Exception("Estabelecimento não encontrado");
+    }
     return service.currentUser();
   }
 }
--- lib/app/modules/company/login/login_controller.dart
+++ lib/app/modules/company/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:default_app/app/shared/repositories/parse_company_service.dart';
 import 'package:default_app/app/shared/repositories/parse_login_service.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginController {
   var loginService = ParseLoginService();
@@ -9,8 +10,11 @@ class LoginController {
   signIn(email, password) async {
     var result = await loginService.signIn(email, password);
     ParseUser currentUser = await ParseUser.currentUser();
-    var adminCompany = await companyService.userHasCompany(currentUser);
-    if (!adminCompany) {
+    var hasCompany = await companyService.userHasCompany(currentUser);
+    if (hasCompany) {
+      var pref = await SharedPreferences.getInstance();
+      pref.setBool("hasCompany", hasCompany);
+    } else {
       await currentUser.logout();
       throw Exception("Estabelecimento não encontrado");
     }

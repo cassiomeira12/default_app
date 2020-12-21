4268e3cd23fb011ed77200da658fcff8c856c03e
Cássio Meira Silva <cassiomeira_12@hotmail.com>
2020-12-21 08:41:58 -0300

    feat: add google fonts

--- lib/app/style/font_style.dart
+++ lib/app/style/font_style.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 TextStyle fontTitle(context, {double size, Color color, bool bold = false}) {
-  return Theme.of(context).textTheme.headline4.copyWith(
-        fontSize: size ?? null,
-        color: color ?? null,
-        fontWeight: bold ? FontWeight.bold : FontWeight.normal,
-      );
+  return GoogleFonts.lato(
+    textStyle: Theme.of(context).textTheme.headline4,
+    color: color ?? null,
+    fontWeight: bold ? FontWeight.bold : FontWeight.normal,
+  );
 }
 
 TextStyle fontSubtitle(context, {double size, Color color, bool bold = false}) {
-  return Theme.of(context).textTheme.headline5.copyWith(
-        fontSize: size ?? null,
-        color: color ?? null,
-        fontWeight: bold ? FontWeight.bold : FontWeight.normal,
-      );
+  return GoogleFonts.lato(
+    textStyle: Theme.of(context).textTheme.headline5,
+    color: color ?? null,
+    fontWeight: bold ? FontWeight.bold : FontWeight.normal,
+  );
 }
 
 TextStyle fontMessage(context, {double size, Color color, bool bold = false}) {
-  return Theme.of(context).textTheme.headline6.copyWith(
-        fontSize: size ?? null,
-        color: color ?? null,
-        fontWeight: bold ? FontWeight.bold : FontWeight.normal,
-      );
+  return GoogleFonts.lato(
+    textStyle: Theme.of(context).textTheme.headline6,
+    color: color ?? null,
+    fontWeight: bold ? FontWeight.bold : FontWeight.normal,
+  );
 }
